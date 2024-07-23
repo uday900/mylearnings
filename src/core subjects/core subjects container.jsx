a9b39c2c2a2f187e0186c1339ef034f4acc8bbda
unknown <udaykirandarla2002@gmail.com>
2024-07-23 11:39:13 +0530

    added ML topics

--- src/core subjects/core subjects container.jsx
+++ src/core subjects/core subjects container.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import JavaFiles from "./java files";
 import ReactjsFiles from "./reactjs files";
+import MachineLearningFiles from "./machine learning files";
 const CourseSubjectContainer = ()=>{
 
 const [enableJava, setenableJava] = useState(false);
 const [enableReactjs, setenableReactjs] = useState(false);
+const [enableML, setenableML] = useState(false);
 return <>
 <div className="container shadow p-2">
             {/* subject 1 */}
@@ -13,15 +15,23 @@ return <>
                     setenableJava(enableJava ? false : true);
                 }
             }>  Java</a>
-            {enableJava && <JavaFiles/>}
-            <br />
+            {enableJava && <JavaFiles/>}<br />
+
             {/* subject 2 */}
             <a href="#"
                 onClick={()=>{
                     setenableReactjs(enableReactjs ? false : true);
                 }
             }>  React.js</a>
-            {enableReactjs&& <ReactjsFiles/>}
+            {enableReactjs&& <ReactjsFiles/>} <br />
+
+            {/* subject -3 */}
+            <a href="#"
+                onClick={()=>{
+                    setenableML(enableML ? false : true);
+                }
+            }>  Machine Learning</a>
+            {enableML&& <MachineLearningFiles/>}
         </div>
     
     </>
